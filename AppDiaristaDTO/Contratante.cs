a723b93290e3c481caa91864b40933d7036606f2
João Pedro Silva <joa.silva@reply.com>
2019-04-23 01:14:22 -0300

    Criação de algumas DTOs e das controllers de diarista e contratante

---
+++ AppDiaristaDTO/Contratante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AppDiaristaDTO
+{
+    public class Contratante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Cep { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
